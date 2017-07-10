7f33208f55a989275c9c234a84171da64ec22b8b
bbungsang <821heejin@naver.com>
2017-07-10 15:09:02 +0900

    views 패키지 생성 fbv.py, cbv.py 분리, 클래스 기반 뷰 적용

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list, name='snippet_list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet_detail'),
+    # fbv urls
+    # url(r'^snippets/$', views.snippet_list, name='snippet_list'),
+    # url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet_detail'),
+
+    # cbv urls
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
